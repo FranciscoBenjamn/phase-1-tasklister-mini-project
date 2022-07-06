4238f2cb073e74215684f4ed9f547573d2537329
Franklin Benjamin <franciscovibes14@gmail.com>
2022-07-05 23:01:26 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let value = e.target["new-task-description"].value
+    let value2 = e.target["due-date"].value
+    taskHandler(value, value2)
+    document.querySelector('#new-task-description').value = ""
+    document.querySelector('#due-date').value = ""
+  })
+})
+
+function taskHandler(taskName, dueDate){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', (e) => {
+    btn.parentElement.remove()
+  })
+
+  btn.textContent = ' x '
+  btn.style="margin-left:5px"
+  li.textContent = `${taskName} is due by ${dueDate}`
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
